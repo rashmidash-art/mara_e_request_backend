a76ce5504fada05d0e6e37c32c7382d39eb4ad3f
rashmidash-art <rashmi.dash@quocent.com>
2025-10-10 11:10:20 +0530

    10/10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+
+        // ]);
+
+
+        // $this->call(class: AdminSeeder::class);
+        // $this->call(PermissionSeeder::class);
+        // $this->call(RemoveEntityPermissionsSeeder::class);
     }
 }
